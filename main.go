cdbfa4659d57d1b33920eec4d0a5351fd4ca5e17
“twinkleaP” <twinklekumari12@gmail.com>
2025-08-22 11:32:46 -0400

    added snippet

--- main.go
+++ main.go
@@ -25,6 +25,22 @@ func main() {
 
 		c.JSON(http.StatusOK, gin.H{"result": a + b})
 	})
+
+	// Route: substraction
+	r.GET("/add", func(c *gin.Context) {
+		aStr := c.Query("a")
+		bStr := c.Query("b")
+
+		a, err1 := strconv.Atoi(aStr)
+		b, err2 := strconv.Atoi(bStr)
+
+		if err1 != nil || err2 != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input, please provide two integers."})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"result": a - b})
+	})
 	// Start server
 	r.Run(":8080")
 }
